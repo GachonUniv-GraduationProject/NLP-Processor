7dbc1fbc3e71db4244537376859433dda675914e
DecisionDisorder <guswhd990507@naver.com>
2022-11-28 19:26:30 +0900

    221128_NLP_socket+service

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import json
+import socket
+from _thread import *
+from nlp_process import classify_pos_neg_sentences, classify_fields
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def threaded(client_socket, addr):
+    print(">> Connected by ", addr[0], ":", addr[1])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                print(">> Disconnected by ", addr[0], ":", addr[1])
+                break
+
+            print(">> Received from ", addr[0], ":", addr[1])
+
+            received_data = data.decode('utf-8')
+            received_data = json.loads(received_data)
+            print(">> Received message: ", received_data)
+
+            if received_data["type"] == "NLP_POS_NEG":
+                result = classify_pos_neg_sentences(received_data["data"])
+            elif received_data["type"] == "NLP_FIELD":
+                result = classify_fields(received_data["data"])
+
+            result = json.dumps(result, ensure_ascii=False)
+            print(result)
+            client_socket.send(result.encode('utf-8'))
+        except ConnectionResetError as e:
+            print(">> Disconnected by ", addr[0], ":", addr[1])
+            break
+
+    if client_socket in client_sockets:
+        client_sockets.remove(client_socket)
+        print('Remove Client List: ', len(client_sockets))
+
+    client_socket.close()
+
+
+client_sockets = []
+
+HOST = '127.0.0.1'
+PORT = 9999
+
+print(">> NLP Processor Start")
+processor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+processor_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+processor_socket.bind((HOST, PORT))
+processor_socket.listen()
+
+try:
+    while True:
+        print(">> Waiting")
+
+        client_socket, addr = processor_socket.accept()
+        client_sockets.append(client_socket)
+        start_new_thread(threaded, (client_socket, addr))
+        print("Clients: ", len(client_sockets))
+
+except Exception as e:
+    print("[Error] ", e)
+
+finally:
+    processor_socket.close()
---
+++ nlp_process.py
@@ -0,0 +1,131 @@
+import json
+from konlpy.tag import Okt
+
+fieldName = ['ai', 'android', 'backend', 'frontend', 'bigdataengineer', 'blockchain', 'dataengineer',
+          'datascience', 'deeplearning', 'gameclient', 'gameserver', 'machinelearning']
+fields = []
+
+
+def load_jsons():
+    with open('fieldData/ai.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/android.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/backend.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/frontend.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/bigdataengineer.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/blockchain.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/dataengineer.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/datascience.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/deeplearning.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/gameclient.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/gameserver.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+    with open('fieldData/machinlearning.json', 'r', encoding="UTF-8") as f:
+        fields.append(json.dumps(json.load(f), ensure_ascii = False))
+
+
+def define_pos_neg(input_split):
+    posEx = ['있', '좋', '느꼈', '느낌', '값진', '경험']
+    negEx = ['없', '못', '싫', '않', '도저히', '아니']
+    result = []
+
+    okt = Okt()
+
+    for i in input_split:
+        direction = 1
+        count = 0
+        morph_list = okt.morphs(i)
+        for m in morph_list:
+            for p in posEx:
+                if m.find(p) != -1:
+                    count = count + 1
+                    direction = 1
+                    break
+            for n in negEx:
+                if m.find(n) != -1:
+                    count = count - 1 * direction
+                    direction = direction * -1
+                    break
+
+        if count > 0:
+            result.append(1)
+        elif count < 0:
+            result.append(-1)
+        else:
+            result.append(0)
+
+    return result
+
+
+def define_field(input_split):
+    okt = Okt()
+    fields_result = []
+
+    for i in input_split:
+        field_define = [0,0,0,0,0,0,0,0,0,0,0,0]
+        for k in okt.phrases(i):
+            for j in range(0, len(fields)):
+                if fields[j].find(k)!=-1:
+                    field_define[j] = field_define[j] + 1
+        fields_result.append(fieldName[field_define.index(max(field_define))])
+
+    return fields_result
+
+
+def ho_bulho(input_split):
+    ho_list = []
+    joonglip_list = []
+    bulho_list = []
+
+    for i in range(0, len(input_split)):
+        if define_pos_neg(input_split)[i] == 1:
+            ho_list.append(define_field(input_split)[i])
+        elif define_pos_neg(input_split)[i] == 0:
+            joonglip_list.append(define_field(input_split)[i])
+        else:
+            bulho_list.append(define_field(input_split)[i])
+
+    return ho_list, joonglip_list, bulho_list
+
+
+def classify_pos_neg_sentences(sentences):
+    sentence_split_list = sentences.split(".")
+    classify = define_pos_neg(sentence_split_list)
+    field = define_field(sentence_split_list)
+    result = {
+        "classify": []
+    }
+    for i in range(len(classify)):
+        res_each = {
+            "field": field[i],
+            "value": classify[i]
+        }
+        result["classify"].append(res_each)
+
+    return result
+
+
+def classify_fields(sentences):
+    sentence_split_list = sentences.split(".")
+    fields_result = define_field(sentence_split_list)
+    result = {
+        "classify": []
+    }
+
+    for i in range(len(fields_result)):
+        res_each = {
+            "field": fields_result[i],
+            "sentence": sentence_split_list[i]
+        }
+        result["classify"].append(res_each)
+
+    return result
